1ea7252445119034eda7b66a69dfb1b89360ed4c
KellyPT <trankphuong@gmail.com>
2017-12-25 16:20:47 -0800

    Style Weather components

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,19 +1,8 @@
 var React = require('react');
 
-// var WeatherMessage = React.createClass({
-//   render: function () {
-//     // var location = this.props.location;
-//     // var temp = this.props.temp;
-//     var {temp, location} = this.props; // ES6 destructuring
-//     return (
-//       <h3>It is {temp} degree in {location}.</h3>
-//     );
-//   }
-// });
-
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h3>It is {temp} degree in {location}.</h3>
+    <h3 className="text-center">It is {temp} degree in {location}.</h3>
   );
 };
 
