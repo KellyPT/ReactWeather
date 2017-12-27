b06e3f0191a3ca2a4e85ee5accb10f919643db95
KellyPT <trankphuong@gmail.com>
2017-12-26 23:10:25 -0800

    Styling for all components

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,18 +1,10 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>About Component</h3>
-//     );
-//   }
-// });
-
 // stateless functional component
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>This is a weather application built on React. I have built this for The Complete React Web App Developer Course.</p>
       <p>Here are some of the tools I used:</p>
       <ul>
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,18 +1,10 @@
 var React = require('react');
 var {Link} = require('react-router');
 
-// var Examples = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>Examples Component</h3>
-//     );
-//   }
-// });
-
 var Examples = (props) => {
   return (
     <div>
-      <h1 className="text-center">Examples</h1>
+      <h1 className="text-center page-title">Examples</h1>
       <p>Here are a few example location to try out:</p>
       <ol>
         <li>
